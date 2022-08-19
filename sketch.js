49f2f01a0fec35da467fd2b47101cc5ece0d5b36
SanzLau <85967982+SanzLau@users.noreply.github.com>
2022-08-18 21:03:12 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,15 @@
+//Crear variable
+var box;
+
+function setup() {
+  createCanvas(400,400);
+  box = createSprite(200,200,30,30);
+
+}
+
+function draw() {
+  background(220);
+
+  //Dibuja los Sprites
+  drawSprites();
+}
